c0a50bb32f8052ee1580c4e62c513ad3322a79e3
Syamsuddin Nur <syamsuddinnur.sn@gmail.com>
2023-11-13 20:43:04 +0700

    CAS-6

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -4,14 +4,18 @@ import { createSlice } from '@reduxjs/toolkit'
 const userSlice = createSlice({
     name: 'user',
     initialState: {
-        value: {}
+        value: {},
+        profile_picture: null,
     },
     reducers: {
         setData: (state, action) => {
             state.value = action.payload
-        }
+        },
+        updateProfilePicture: (state, action) => {
+            state.value.profile_picture = action.payload;
+          },
     }
 })
 
-export const { setData } = userSlice.actions
+export const { setData, updateProfilePicture } = userSlice.actions
 export default userSlice.reducer
